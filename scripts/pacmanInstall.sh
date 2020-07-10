1c82dd85690fde19afe6fa2ccdd8adff6a92de99
Cody Bender <codyfbender@gmail.com>
2020-07-10 00:07:02 -0600

    Change i3 keybinds and add bashtop

--- scripts/pacmanInstall.sh
+++ scripts/pacmanInstall.sh
@@ -26,12 +26,12 @@ sudo pacman -Syyu --noconfirm
 
 # Terminal setup
 # Fish and fisher
-install -S fish
+install fish
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 # Alacritty
-install -S alacritty
+install alacritty
 # neovim
-install -S neovim
+install neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # aura
@@ -43,12 +43,14 @@ cd ..
 rm -rf aura-bin
 cd $oldDir
 # tmux and tpm
-install -S tmux
+install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # ripgrep
-install -S ripgrep
+install ripgrep
 # screenshot util
-install -S maim xclip
+install maim xclip
+# bashtop
+install bashtop
 
 # ranger dev icons
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons

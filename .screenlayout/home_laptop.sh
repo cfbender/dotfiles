11532086a689c64946a726ff47006abfc4934df2
Cody Bender <cody.bender@pdq.com>
2020-10-28 09:24:29 -0600

    feat: add gpg

---
+++ .screenlayout/home_laptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1.1 --mode 1920x1080 --pos 2560x180 --rotate normal --output DP-1.3 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --off

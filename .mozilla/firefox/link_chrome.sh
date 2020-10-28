56a71084baa2870ce255ed5c093b5cb43ae994af
Cody Bender <cody.bender@pdq.com>
2020-10-28 09:30:07 -0600

    fix: script and css

--- .mozilla/firefox/link_chrome.sh
+++ .mozilla/firefox/link_chrome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 firefoxDir=$(find ~/.mozilla/firefox/ -name "*.default-release" -type d)
-
-ln -s ./chrome/userChrome.css "${firefoxDir}/chrome/userChrome.css"
+currentDir=$(pwd)
+ln -s "${currentDir}/chrome/userChrome.css" "${firefoxDir}/chrome/userChrome.css"

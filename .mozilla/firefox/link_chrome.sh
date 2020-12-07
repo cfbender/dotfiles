c5f336cdb8a91f75c79170cf262cbe0241a16d15
Cody Bender <cody.bender@pdq.com>
2020-12-07 16:55:52 -0700

    chore: fix setup

--- .mozilla/firefox/link_chrome.sh
+++ .mozilla/firefox/link_chrome.sh
@@ -2,4 +2,10 @@
 
 firefoxDir=$(find ~/.mozilla/firefox/ -name "*.default-release" -type d)
 currentDir=$(pwd)
+
+if [ ! -d "${firefoxDir}/chrome" ]; then
+echo "Creating nonexistent directory: ${firefoxDir}/chrome"
+mkdir -p ${firefoxDir}/chrome
+fi
+
 ln -s "${currentDir}/chrome/userChrome.css" "${firefoxDir}/chrome/userChrome.css"
--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -3,3 +3,6 @@
 # Desktop stuff
 sudo pacman -S i3-gaps rofi picom nitrogen morc_menu dmenu wmctrl bmenu polybar
 
+sudo cp ../plasma-i3.desktop /usr/share/xsessions/
+
+sudo xrdb ~/.Xresources

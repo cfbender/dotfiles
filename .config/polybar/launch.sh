46168162aebe05d3ed115a63d8181b6a93b1dc38
Cody Bender <cody.bender@pdq.com>
2021-02-15 09:34:29 -0700

    chore: laptop screen fixes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,9 +4,9 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-export MONITOR1=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
-export MONITOR2=$(xrandr -q | grep " connected primary" | cut -d ' ' -f1 | head -n 1)
-export MONITOR3=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 1p)
+export MONITOR3=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 3p)
+export MONITOR1=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 1p)
+export MONITOR2=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
 
 polybar bar &
 polybar second &
--- .screenlayout/home_laptop.sh
+++ .screenlayout/home_laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1.1 --mode 1920x1080 --pos 2560x180 --rotate normal --output DP-1.3 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --off
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output DP-1.1 --mode 2560x1440 --pos 4000x560 --rotate normal --output DP-1.2 --primary --mode 2560x1440 --pos 1440x560 --rotate normal --output DP-1.3 --mode 2560x1440 --pos 0x0 --rotate left

f2a77820c43da34b1757ffc0ab5d35e3a7a8b8e6
Cody Bender <cody.bender@pdq.com>
2021-06-16 09:41:01 -0600

    fix: only show bars if monitors are present

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,12 +4,12 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-export MONITOR3=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 3p)
 export MONITOR1=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 1p)
 export MONITOR2=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
+export MONITOR3=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 3p)
 
 polybar bar &
-polybar second &
-polybar third &
+if [[ MONITOR2 ]]; then polybar second & echo "Second bar on $MONITOR2"; fi &
+if [[ MONITOR3 ]]; then polybar third & echo "Third bar on $MONITOR3"; fi
 
 echo "Bars launched..."

9a7e81f5a0ef651ffa8df7b99beea88fb1083fc8
Cody Bender <cody.bender@pdq.com>
2021-11-19 16:47:32 -0700

    chore: macOS changes
    feat: add mac updates
    
    fix: nvm on mac
    
    chore: set git defaultBranch and add apollo rover to path
    
    chore: update fish and ranger config
    
    chore: ranger config
    
    chore: update config
    
    chore: update config
    
    chore: update rnvimr cmd and add pnpm to path
    
    feat: add clear-dns
    
    chore: update gpg settings
    
    chore: remove echo for pinentry
    
    chore: update config
    
    chore: remove nvm node version
    
    chore: git files correct
    
    chore: add haskell to fish
    
    chore: add brew to zprofile
    
    chore: add asdf to plugins for mac
    
    fix: zprofile homebrew
    
    chore: fix zprofile
    
    chore: update mac zshrc
    
    chore: add bup
    
    chore: mise config

--- .mozilla/firefox/link_chrome.sh
+++ .mozilla/firefox/link_chrome.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-firefoxDir=$(find ~/.mozilla/firefox/ -name "*.default-release" -type d)
+firefoxDir=$(find /Users/$(whoami)/Library/Application\ Support/Firefox/Profiles -name "*.default-release" -type d)
 currentDir=$(pwd)
 
+if [[ -z "${firefoxDir}" ]]; then
+  echo "Unable to find firefox installation"
+  exit 420
+fi
+
 if [ ! -d "${firefoxDir}/chrome" ]; then
-echo "Creating nonexistent directory: ${firefoxDir}/chrome"
-mkdir -p ${firefoxDir}/chrome
+  echo "Creating nonexistent directory: ${firefoxDir}/chrome"
+  mkdir ${firefoxDir}/chrome
 fi
 
 ln -s "${currentDir}/chrome/userChrome.css" "${firefoxDir}/chrome/userChrome.css"

c4a7c4db9b1401582b60615b80f3aa68eb8f3241
Cody Bender <cody.bender@pdq.com>
2021-11-19 17:42:28 -0700

    feat: add mac updates

---
+++ scripts/macInstall.sh
@@ -0,0 +1,22 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install \
+  git \
+  node \
+  elixir \
+  bpytop \
+  bat \
+  ripgrep \
+  fd \
+  tmux \
+  asdf
+
+brew install --cask \
+  chromium \
+  hyperdock \
+  bitwarden \
+  iterm2 \
+  authy \
+  stats \
+  gpg-suite
+

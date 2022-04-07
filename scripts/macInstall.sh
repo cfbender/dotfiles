e9b0cb2ab325105bc6f6b6840c98dc7c045950b0
Cody Bender <cody.bender@pdq.com>
2022-04-07 11:49:39 -0600

    chore: ranger config

--- scripts/macInstall.sh
+++ scripts/macInstall.sh
@@ -9,6 +9,7 @@ brew install \
   ripgrep \
   fd \
   tmux \
+  duti \
   asdf
 
 brew install --cask \
@@ -20,3 +21,70 @@ brew install --cask \
   stats \
   gpg-suite
 
+duti -s nvim public.json all
+  duti -s nvim public.plain-text all
+  duti -s nvim public.python-script all
+  duti -s nvim public.shell-script all
+  duti -s nvim public.source-code all
+  duti -s nvim public.text all
+  duti -s nvim public.unix-executable all
+  duti -s nvim .c all
+  duti -s nvim .cpp all
+  duti -s nvim .cs all
+  duti -s nvim .css all
+  duti -s nvim .ex all
+  duti -s nvim .heex all
+  duti -s nvim .go all
+  duti -s nvim .java all
+  duti -s nvim .js all
+  duti -s nvim .sass all
+  duti -s nvim .scss all
+  duti -s nvim .less all
+  duti -s nvim .vue all
+  duti -s nvim .cfg all
+  duti -s nvim .json all
+  duti -s nvim .jsx all
+  duti -s nvim .log all
+  duti -s nvim .lua all
+  duti -s nvim .md all
+  duti -s nvim .php all
+  duti -s nvim .pl all
+  duti -s nvim .py all
+  duti -s nvim .rb all
+  duti -s nvim .ts all
+  duti -s nvim .tsx all
+  duti -s nvim public.json all
+  duti -s nvim public.plain-text all
+  duti -s nvim public.python-script all
+  duti -s nvim public.shell-script all
+  duti -s nvim public.source-code all
+  duti -s nvim public.text all
+  duti -s nvim public.unix-executable all
+  duti -s nvim .c all
+  duti -s nvim .cpp all
+  duti -s nvim .cs all
+  duti -s nvim .css all
+  duti -s nvim .go all
+  duti -s nvim .java all
+  duti -s nvim .js all
+  duti -s nvim .sass all
+  duti -s nvim .scss all
+  duti -s nvim .less all
+  duti -s nvim .vue all
+  duti -s nvim .cfg all
+  duti -s nvim .json all
+  duti -s nvim .jsx alll
+  duti -s nvim .log all
+  duti -s nvim .lua all
+  duti -s nvim .md all
+  duti -s nvim .php all
+  duti -s nvim .pl all
+  duti -s nvim .py all
+  duti -s nvim .rb all
+  duti -s nvim .ts all
+  duti -s nvim .tsx all
+  duti -s nvim .txt all
+  duti -s nvim .conf all
+  duti -s nvim .yaml all
+  duti -s nvim .yml all
+  duti -s nvim .toml all

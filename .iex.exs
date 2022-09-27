6e8ea960691595d340608abb11f3e869a38ff298
Cody Bender <cody.bender@pdq.com>
2022-09-27 16:49:39 -0400

    feat: add global iex configuration

---
+++ .iex.exs
@@ -0,0 +1,41 @@
+import_if_available(Ecto.Query)
+import_if_available(Ecto.Changeset)
+
+defmodule AC do
+  def update(schema, changes) do
+    schema
+    |> Ecto.Changeset.change(changes)
+    |> Repo.update()
+  end
+
+  IEx.configure(
+    colors: [
+      syntax_colors: [
+        number: :light_yellow,
+        atom: :light_cyan,
+        string: :light_black,
+        boolean: [:light_blue],
+        nil: [:magenta, :bright]
+      ],
+      ls_directory: :cyan,
+      ls_device: :yellow,
+      doc_code: :green,
+      doc_inline_code: :magenta,
+      doc_headings: [:cyan, :underline],
+      doc_title: [:cyan, :bright, :underline]
+    ],
+    default_prompt:
+      [
+        # ANSI CHA, move cursor to column 1
+        "\e[G",
+        :light_magenta,
+        # plain string
+        " iex",
+        ">",
+        :white,
+        :reset
+      ]
+      |> IO.ANSI.format()
+      |> IO.chardata_to_string()
+  )
+end

121ef7fc9628ccacf56010ccb2f8a871f2eb9858
Cody Bender <cody.bender@pdq.com>
2022-10-10 01:49:32 -0400

    chore: add yamlls settings

--- .config/nvim/lua/user/lsp/server-settings/yamlls.lua
+++ .config/nvim/lua/user/lsp/server-settings/yamlls.lua
@@ -2,8 +2,10 @@ return { -- override table for require("lspconfig").yamlls.setup({...})
 	settings = {
 		yaml = {
 			schemas = {
-				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+				["https://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+				["https://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+				["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.*.{yml,yaml}",
+				["https://json.schemastore.org/traefik-v2"] = "traefik.{yml,yaml}",
 			},
 		},
 	},
--- .config/nvim/lua/user/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/user/plugins/mason-lspconfig.lua
@@ -1,5 +1,5 @@
 -- use mason-lspconfig to configure LSP installations
 return { -- overrides `require("mason-lspconfig").setup(...)`
 	automatic_installation = true,
-	ensure_installed = { "tsserver", "elixir-ls", "rust_analyzer", "sumneko_lua" },
+	ensure_installed = { "tsserver", "elixir-ls", "rust_analyzer", "sumneko_lua", "yamlls" },
 }

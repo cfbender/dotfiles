d0565d69519c580b6963de09e5aac47807e0b35b
Cody Bender <cody.bender@pdq.com>
2022-11-21 19:00:34 -0500

    feat: add html-ls for heex and switch to stable

--- .config/nvim/lua/user/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/user/plugins/mason-lspconfig.lua
@@ -1,5 +1,5 @@
 -- use mason-lspconfig to configure LSP installations
 return { -- overrides `require("mason-lspconfig").setup(...)`
 	automatic_installation = true,
-	ensure_installed = { "tsserver", "elixirls", "rust_analyzer", "sumneko_lua", "yamlls" },
+	ensure_installed = { "tsserver", "elixirls", "rust_analyzer", "sumneko_lua", "yamlls", "html" },
 }

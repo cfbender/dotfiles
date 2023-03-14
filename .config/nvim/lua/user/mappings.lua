5fe2b58abe02026713c047b23356e1bb7112d645
Cody Bender <cody.bender@pdq.com>
2023-03-14 15:06:56 -0400

    chore(nvim): remove unused header file and change back buffer switch keybinds

--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -4,44 +4,64 @@
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
-    -- hop.nvim
-    ["<leader><space>"] = { name = "Hop" },
-    ["<leader><leader>w"] = { "<cmd>HopWord<cr>", desc = "Hop to a word" },
-    ["<leader><leader>p"] = { "<cmd>HopPattern<cr>", desc = "Hop to a pattern" },
-    ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
-    ["<leader>gd"] = { "<cmd>DiffviewOpen<cr>", desc = "View git diff in Diffview" },
-    ["<leader>gdc"] = { "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
-    ["<leader>ff"] = { "<cmd>Telescope git_files<cr>", desc = "Search all files in git" },
-    ["<leader>fr"] = { "<cmd>Telescope resume<cr>", desc = "Resume previous telescope search" },
-    ["<leader>lg"] = { "<cmd>Neogen<cr>", desc = "Generate annotation for the current node" },
-    -- resize with arrows
-    ["<Up>"] = {
-      function() require("smart-splits").resize_up(2) end,
-      desc = "Resize split up",
-    },
-    ["<Down>"] = {
-      function() require("smart-splits").resize_down(2) end,
-      desc = "Resize split down",
-    },
-    ["<Left>"] = {
-      function() require("smart-splits").resize_left(2) end,
-      desc = "Resize split left",
-    },
-    ["<Right>"] = {
-      function() require("smart-splits").resize_right(2) end,
-      desc = "Resize split right",
-    },
-    [","] = {
-      "@@",
-      desc = "Replay last used macro",
-    },
-    ["<leader>b"] = { name = "Buffer" },
-  },
-  t = {
-    ["<c-q>"] = { "<c-\\><c-n>", desc = "Terminal normal mode" },
-    ["<esc><esc>"] = { "<c-\\><c-n>:q<cr>", desc = "Terminal quit" },
-  },
+	-- first key is the mode
+	n = {
+		-- second key is the lefthand side of the map
+		-- hop.nvim
+		["<leader><space>"] = { name = "Hop" },
+		["<leader><leader>w"] = { "<cmd>HopWord<cr>", desc = "Hop to a word" },
+		["<leader><leader>p"] = { "<cmd>HopPattern<cr>", desc = "Hop to a pattern" },
+		["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
+		["<leader>gd"] = { "<cmd>DiffviewOpen<cr>", desc = "View git diff in Diffview" },
+		["<leader>gdc"] = { "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+		["<leader>ff"] = { "<cmd>Telescope git_files<cr>", desc = "Search all files in git" },
+		["<leader>fr"] = { "<cmd>Telescope resume<cr>", desc = "Resume previous telescope search" },
+		["<leader>lg"] = { "<cmd>Neogen<cr>", desc = "Generate annotation for the current node" },
+		-- resize with arrows
+		["<Up>"] = {
+			function()
+				require("smart-splits").resize_up(2)
+			end,
+			desc = "Resize split up",
+		},
+		["<Down>"] = {
+			function()
+				require("smart-splits").resize_down(2)
+			end,
+			desc = "Resize split down",
+		},
+		["<Left>"] = {
+			function()
+				require("smart-splits").resize_left(2)
+			end,
+			desc = "Resize split left",
+		},
+		["<Right>"] = {
+			function()
+				require("smart-splits").resize_right(2)
+			end,
+			desc = "Resize split right",
+		},
+		[","] = {
+			"@@",
+			desc = "Replay last used macro",
+		},
+		["<leader>b"] = { name = "Buffer" },
+		L = {
+			function()
+				require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+			end,
+			desc = "Next buffer",
+		},
+		H = {
+			function()
+				require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+			end,
+			desc = "Previous buffer",
+		},
+	},
+	t = {
+		["<c-q>"] = { "<c-\\><c-n>", desc = "Terminal normal mode" },
+		["<esc><esc>"] = { "<c-\\><c-n>:q<cr>", desc = "Terminal quit" },
+	},
 }

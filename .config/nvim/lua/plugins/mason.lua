4f37262b7a4075bc9770097c3c49dde4f1fcb07e
Cody Bender <cody.bender@pdq.com>
2024-04-02 10:59:42 -0700

    chore: swap elixirls for lexical

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -12,7 +12,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "tsserver",
-        "elixirls",
+        "lexical",
         "rust_analyzer",
         "lua_ls",
         "yamlls",

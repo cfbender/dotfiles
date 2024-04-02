825868f88dd4a2365edafeef2af3037d2d169fe0
Cody Bender <cody.bender@pdq.com>
2024-04-02 10:56:17 -0700

    fix: remove old file and add d

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -29,7 +29,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
+        "prettierd",
         "eslint_d",
         "stylua",
         -- add more arguments for adding more null-ls sources

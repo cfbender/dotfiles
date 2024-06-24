a2a81367112f6e46b285cba1a530d418a12cab6a
Cody Bender <cody.bender@pdq.com>
2024-06-24 15:53:53 -0400

    chore: conf

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -7,5 +7,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.motion.flash-nvim" },
-  -- import/override with your plugins folder
+  -- { import = "astrocommunity.completion.copilot-cmp" },
 }

312456e4d8ab8793308a47bd745c6cbcc9781527
Cody Bender <cody.bender@pdq.com>
2024-07-09 14:22:18 -0700

    chore: add rally script to dotfiles

--- .local/bin/rally.sh
+++ .local/bin/rally.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# credit to @skbolton
+
 set -eu
-TARGET=$(ls -d ~/code/pdq/* ~/code/github/*  ~/code/playground/* ~/.config/* | fzf --header-first --header="Launch Project" --preview "eza --color=always --tree --icons --level 3 --git-ignore {}")
+TARGET=$(ls -d ~/code/pdq/* ~/code/github ~/code/playground | gum filter --limit 1 --placeholder 'yee ur last haw partner' --height 50 --prompt='  ')
 NAME=$(basename $TARGET)
-SESSION_NAME=$(echo $NAME)
+SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:])
 
 if [[ -f "$HOME/.config/smug/$NAME.yml" ]]; then
   smug start $NAME -a

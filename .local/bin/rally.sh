09bbed55f29b355da8a9a6d42903195fbd6f4169
Cody Bender <cody.bender@pdq.com>
2024-07-11 13:42:46 -0700

    chore: fix rally and gh dash

--- .local/bin/rally.sh
+++ .local/bin/rally.sh
@@ -3,7 +3,7 @@
 # credit to @skbolton
 
 set -eu
-TARGET=$(ls -d ~/code/pdq/* ~/code/github ~/code/playground | gum filter --limit 1 --placeholder 'yee ur last haw partner' --height 50 --prompt='  ')
+TARGET=$(ls -d ~/code/pdq/* ~/code/github/* ~/code/playground/* | gum filter --limit 1 --placeholder 'yee ur last haw partner' --height 50 --prompt='  ')
 NAME=$(basename $TARGET)
 SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:])
 

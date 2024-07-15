85df06b4a4e6f057cc75d73ebe8a5149cdaa68fc
Cody Bender <cody.bender@pdq.com>
2024-07-15 16:07:11 -0400

    chore: fix

--- .local/bin/rally.sh
+++ .local/bin/rally.sh
@@ -3,7 +3,7 @@
 # credit to @skbolton
 
 set -eu
-TARGET=$(ls -d ~/code/pdq/* ~/code/github/* ~/code/playground/* | gum filter --limit 1 --placeholder 'yee ur last haw partner' --height 50 --prompt='  ')
+TARGET=$(ls -d ~/code/pdq/* ~/code/github/* ~/code/playground/* | gum filter --limit 1 --placeholder 'yee ur last haw' --height 50 --prompt='  ')
 NAME=$(basename $TARGET)
 SESSION_NAME=$(echo $NAME | tr [:lower:] [:upper:])
 

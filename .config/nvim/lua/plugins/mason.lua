ca65e33dfa9ec750cebc0e325254e57ffdd84422
Cody Bender <cody.bender@pdq.com>
2024-09-09 09:17:58 -0700

    chore: bat and mason config

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "tsserver",
+        "ts_ls",
         "lexical",
         "rust_analyzer",
         "lua_ls",

c0b46b7bbb63435271b3cdb237d6e181d923d77e
Cody Bender <cody.bender@pdq.com>
2024-10-14 14:50:47 -0400

    chore: back to catppuccin my love

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -146,7 +146,30 @@ return {
   "shaunsingh/nord.nvim",
   "Mofiqul/dracula.nvim",
   "rebelot/kanagawa.nvim",
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup {
+        variant = "main", -- auto, main, moon, or dawn
+        dark_variant = "main", -- main, moon, or dawn
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+
+        enable = {
+          terminal = true,
+          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+          migrations = true, -- Handle deprecated options automatically
+        },
+
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = false,
+        },
+      }
+    end,
+  },
   {
     "catppuccin/nvim", -- Soothing pastel theme for Neovim
     name = "catppuccin",
--- .wezterm.lua
+++ .wezterm.lua
@@ -1,19 +1,22 @@
 local wezterm = require("wezterm")
+-- for rosepine
+-- local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
+local flavor = "Catppuccin Frappe"
+local theme = wezterm.color.get_builtin_schemes()[flavor]
 
-local config = wezterm.config_builder()
-local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
+local config = {
+	font = wezterm.font_with_fallback({
+		"0xProto Nerd Font",
+		{ family = "DankMono Nerd Font", weight = "Regular" },
+	}),
+	colors = theme,
+	color_scheme = flavor,
+	-- for rosepine
+	-- window_frame = theme.window_frame(), -- needed only if using fancy tab bar
+	default_prog = {
+		"C:\\Users\\codyf\\AppData\\Local\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe",
+	},
+	window_decorations = "RESIZE",
+}
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Frappe"
-config.font = wezterm.font_with_fallback({
-	"0xProto Nerd Font",
-	"monospace",
-})
-config.font_size = 14.0
-config.colors.theme.colors()
-config.window_frame.theme.window_frame()
-
--- and finally, return the configuration to wezterm
 return config

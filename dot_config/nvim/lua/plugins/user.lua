67009d2e6d0e1eb139a99fa2ba1b4b96c89ca64f
Cody Bender <cody.bender@pdq.com>
2025-03-27 17:45:04 -0400

    chore: config

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -126,6 +126,7 @@ return {
 	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
+		enabled = false,
 		opts = function()
 			return {
 				window = { position = "left" },
@@ -463,6 +464,15 @@ return {
 			open_for_directories = true,
 			keymaps = {
 				show_help = "<f1>",
+				open_file_in_vertical_split = "<c-v>",
+				open_file_in_horizontal_split = "<c-x>",
+				open_file_in_tab = "<c-t>",
+				grep_in_directory = "<c-s>",
+				replace_in_directory = "<c-g>",
+				cycle_open_buffers = "<tab>",
+				copy_relative_path_to_selected_files = "<c-y>",
+				send_to_quickfix_list = "<c-q>",
+				change_working_directory = "<c-\\>",
 			},
 		},
 		-- 👇 if you use `open_for_directories=true`, this is recommended

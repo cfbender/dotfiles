d4c336e848eaa0ba5ec512e33f14994827db67d9
Cody Bender <cody.bender@pdq.com>
2025-03-27 14:18:27 -0400

    chore: fix go to definition using snacks

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -5,153 +5,185 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = false, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = true, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
-    },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      v = {
-        ["<C-c><C-c>"] = {
-          desc = "Send to Vimux",
-          function()
-            -- yank text into v register
-            if vim.api.nvim_get_mode()["mode"] == "n" then
-              vim.cmd 'normal vip"vy'
-            else
-              vim.cmd 'normal "vy'
-            end
-            -- construct command with v register as command to send
-            -- vim.cmd(string.format('call VimuxRunCommand("%s")', vim.trim(vim.fn.getreg('v'))))
-            vim.cmd "call VimuxRunCommand(@v)"
-          end,
-        },
-      },
-      n = {
-        ["\\"] = false,
-        ["|"] = false,
-        ["<C-c><C-c>"] = {
-          desc = "Send to Vimux",
-          function()
-            -- yank text into v register
-            if vim.api.nvim_get_mode()["mode"] == "n" then
-              vim.cmd 'normal vip"vy'
-            else
-              vim.cmd 'normal "vy'
-            end
-            -- construct command with v register as command to send
-            -- vim.cmd(string.format('call VimuxRunCommand("%s")', vim.trim(vim.fn.getreg('v'))))
-            vim.cmd "call VimuxRunCommand(@v)"
-          end,
-        },
-        gl = {
-          function() vim.diagnostic.open_float() end,
-          desc = "Hover diagnostics",
-        },
-        -- second key is the lefthand side of the map
-        ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
-        ["<leader>gj"] = {
-          function() require("gitsigns").next_hunk() end,
-          desc = "Next Git hunk",
-        },
-        ["<leader>gk"] = {
-          function() require("gitsigns").prev_hunk() end,
-          desc = "Previous Git hunk",
-        },
-        ["<leader>ff"] = { function() Snacks.picker.git_files() end, desc = "Find Git Files" },
-        -- resize with arrows
-        ["<Up>"] = {
-          function() require("smart-splits").resize_up(2) end,
-          desc = "Resize split up",
-        },
-        ["<Down>"] = {
-          function() require("smart-splits").resize_down(2) end,
-          desc = "Resize split down",
-        },
-        ["<Left>"] = {
-          function() require("smart-splits").resize_left(2) end,
-          desc = "Resize split left",
-        },
-        ["<Right>"] = {
-          function() require("smart-splits").resize_right(2) end,
-          desc = "Resize split right",
-        },
-        [","] = {
-          "@@",
-          desc = "Replay last used macro",
-        },
-        ["<leader>b"] = { name = "Buffer" },
-        L = {
-          function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-          desc = "Next buffer",
-        },
-        H = {
-          function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-          desc = "Previous buffer",
-        },
-        ["<leader>c"] = {
-          function()
-            local bufs = vim.fn.getbufinfo { buflisted = true }
-            require("astrocore.buffer").close()
-            if require("astrocore").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
-          end,
-          desc = "Close buffer",
-        },
-        -- navigate buffer tabs with `H` and `L`
-        -- L = {
-        --   function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-        --   desc = "Next buffer",
-        -- },
-        -- H = {
-        --   function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-        --   desc = "Previous buffer",
-        -- },
+	"AstroNvim/astrocore",
+	---@type AstroCoreOpts
+	opts = {
+		-- Configure core features of AstroNvim
+		features = {
+			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+			autopairs = true, -- enable autopairs at start
+			cmp = true, -- enable completion at start
+			diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
+			highlighturl = true, -- highlight URLs at start
+			notifications = true, -- enable notifications at start
+		},
+		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+		diagnostics = {
+			virtual_text = true,
+			underline = true,
+		},
+		-- vim options can be configured here
+		options = {
+			opt = { -- vim.opt.<key>
+				relativenumber = false, -- sets vim.opt.relativenumber
+				number = true, -- sets vim.opt.number
+				spell = true, -- sets vim.opt.spell
+				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+				wrap = false, -- sets vim.opt.wrap
+			},
+			g = { -- vim.g.<key>
+				-- configure global vim variables (vim.g)
+				-- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+				-- This can be found in the `lua/lazy_setup.lua` file
+			},
+		},
+		-- Mappings can be configured through AstroCore as well.
+		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+		mappings = {
+			-- first key is the mode
+			v = {
+				["<C-c><C-c>"] = {
+					desc = "Send to Vimux",
+					function()
+						-- yank text into v register
+						if vim.api.nvim_get_mode()["mode"] == "n" then
+							vim.cmd('normal vip"vy')
+						else
+							vim.cmd('normal "vy')
+						end
+						-- construct command with v register as command to send
+						-- vim.cmd(string.format('call VimuxRunCommand("%s")', vim.trim(vim.fn.getreg('v'))))
+						vim.cmd("call VimuxRunCommand(@v)")
+					end,
+				},
+			},
+			n = {
+				["\\"] = false,
+				["|"] = false,
+				["<C-c><C-c>"] = {
+					desc = "Send to Vimux",
+					function()
+						-- yank text into v register
+						if vim.api.nvim_get_mode()["mode"] == "n" then
+							vim.cmd('normal vip"vy')
+						else
+							vim.cmd('normal "vy')
+						end
+						-- construct command with v register as command to send
+						-- vim.cmd(string.format('call VimuxRunCommand("%s")', vim.trim(vim.fn.getreg('v'))))
+						vim.cmd("call VimuxRunCommand(@v)")
+					end,
+				},
+				gl = {
+					function()
+						vim.diagnostic.open_float()
+					end,
+					desc = "Hover diagnostics",
+				},
+				gd = {
+					function()
+						Snacks.picker.lsp_definitions()
+					end,
+					desc = "Go to definition of current symbol",
+				},
+				-- second key is the lefthand side of the map
+				["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bD"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Pick to close",
-        },
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        ["<Leader>b"] = { desc = "Buffers" },
-        -- quick save
-        -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-      },
-    },
-  },
+				["<leader>gj"] = {
+					function()
+						require("gitsigns").nav_hunk("next")
+					end,
+					desc = "Next Git hunk",
+				},
+				["<leader>gk"] = {
+					function()
+						require("gitsigns").nav_hunk("prev")
+					end,
+					desc = "Previous Git hunk",
+				},
+				["<leader>ff"] = {
+					function()
+						Snacks.picker.git_files()
+					end,
+					desc = "Find Git Files",
+				},
+				-- resize with arrows
+				["<Up>"] = {
+					function()
+						require("smart-splits").resize_up(2)
+					end,
+					desc = "Resize split up",
+				},
+				["<Down>"] = {
+					function()
+						require("smart-splits").resize_down(2)
+					end,
+					desc = "Resize split down",
+				},
+				["<Left>"] = {
+					function()
+						require("smart-splits").resize_left(2)
+					end,
+					desc = "Resize split left",
+				},
+				["<Right>"] = {
+					function()
+						require("smart-splits").resize_right(2)
+					end,
+					desc = "Resize split right",
+				},
+				[","] = {
+					"@@",
+					desc = "Replay last used macro",
+				},
+				["<leader>b"] = { name = "Buffer" },
+				L = {
+					function()
+						require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+					end,
+					desc = "Next buffer",
+				},
+				H = {
+					function()
+						require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+					end,
+					desc = "Previous buffer",
+				},
+				["<leader>c"] = {
+					function()
+						local bufs = vim.fn.getbufinfo({ buflisted = true })
+						require("astrocore.buffer").close()
+						if require("astrocore").is_available("alpha-nvim") and not bufs[2] then
+							require("alpha").start(true)
+						end
+					end,
+					desc = "Close buffer",
+				},
+				-- navigate buffer tabs with `H` and `L`
+				-- L = {
+				--   function() require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+				--   desc = "Next buffer",
+				-- },
+				-- H = {
+				--   function() require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+				--   desc = "Previous buffer",
+				-- },
+
+				-- mappings seen under group name "Buffer"
+				["<Leader>bD"] = {
+					function()
+						require("astroui.status.heirline").buffer_picker(function(bufnr)
+							require("astrocore.buffer").close(bufnr)
+						end)
+					end,
+					desc = "Pick to close",
+				},
+				-- tables with just a `desc` key will be registered with which-key if it's installed
+				-- this is useful for naming menus
+				["<Leader>b"] = { desc = "Buffers" },
+				-- quick save
+				-- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+			},
+		},
+	},
 }

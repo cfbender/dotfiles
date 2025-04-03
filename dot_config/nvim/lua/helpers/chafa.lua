f289d9a46b35e2e55183c913bef4523f034a05a9
Cody Bender <cody.bender@pdq.com>
2025-04-03 11:51:09 -0400

    refactor: read from dir

--- dot_config/nvim/lua/helpers/chafa.lua
+++ dot_config/nvim/lua/helpers/chafa.lua
@@ -1,51 +1,52 @@
 local desiredWidth = 60
 
-local function image_size(dimensions)
-	local width, height = dimensions:match("^(%d+)x(%d+)$")
-
-	height = tonumber(height)
-
-	-- find width to match 25px height based on aspect ratio
+local function getHeight(width, height)
 	local aspect_ratio = tonumber(width) / desiredWidth
 	height = math.floor(height / aspect_ratio)
 	if height < 1 then
 		height = 1
 	end
 
-	return { width = desiredWidth, height = height }
+	return height
 end
 
-local function image_data(file, dimensions)
-	local size = image_size(dimensions)
+local function imageData(file)
+	-- get image dimensions from file command
+	local result = vim.fn.system({
+		"file",
+		vim.fn.fnamemodify(file, ":p"),
+	})
+	local width, height = string.match(result, "(%d+)%s*x%s*(%d+)")
+
 	return {
-		height = size.height,
-		cmd = string.format("chafa %s --format symbols --symbols vhalf --size %sx%s", file, size.width, size.height),
+		height = getHeight(width, height),
+		cmd = string.format("chafa %s --format symbols --symbols vhalf --fit-width --view-size %s", file, desiredWidth),
 	}
 end
 
 local table = {}
 
-local data = {
-	{
-		name = "sakura",
-		file = "~/assets/lo-fi-sakura.webp",
-		size = "900x506",
-	},
-	{
-		name = "linville",
-		file = "~/assets/linville.png",
-		size = "1536x1024",
-	},
-}
+local path = vim.fn.expand("~/assets")
+local files = vim.fn.readdir(path)
 
-for _, v in pairs(data) do
-	data = image_data(v.file, v.size)
-	table = vim.tbl_extend("force", table, {
-		[v.name] = {
-			cmd = data.cmd,
-			height = math.ceil(data.height / 2),
-		},
-	})
+local function is_image_file(filename)
+	return filename:lower():match("%.jpe?g$") or filename:lower():match("%.png$") or filename:lower():match("%.webp$")
+end
+local function trim_extension(filename)
+	return filename:match("^(.*)%.[^%.]+$") or filename
+end
+
+for _, file in ipairs(files) do
+	local fullpath = path .. "/" .. file
+	if vim.fn.isdirectory(fullpath) == 0 and is_image_file(file) then
+		local data = imageData(fullpath)
+		table = vim.tbl_extend("force", table, {
+			[trim_extension(file)] = {
+				cmd = data.cmd,
+				height = math.ceil(data.height / 2),
+			},
+		})
+	end
 end
 
 return table

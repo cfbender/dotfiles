aa6a3f81828c463d31c441d26e4d44e00b82e9fd
Cody Bender <cody.bender@pdq.com>
2026-03-13 14:54:59 -0400

    chore: clean unused files

--- dot_screenlayout/executable_home_laptop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output DP-1.1 --mode 2560x1440 --pos 4000x560 --rotate normal --output DP-1.2 --primary --mode 2560x1440 --pos 1440x560 --rotate normal --output DP-1.3 --mode 2560x1440 --pos 0x0 --rotate left
